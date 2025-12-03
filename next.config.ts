f2af35d587e7970cf6b88ae8db7d5325377b86c0
AxelJhostin <88396943+AxelJhostin@users.noreply.github.com>
2025-12-03 10:15:16 -0500

    cambios icono y más

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com', // Le damos permiso a Dicebear
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
